1838bdc02188f01a4d8af59cb225a7ebbf98a6b2
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-07 14:15:59 +0530

    Add some CSS

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Todoform.css"
 
 export default function Todoform() {
   return (
-    <form>
+    <form className='todo-form'>
         <input type='text' placeholder='Enter a Task'/>
         <button>Add</button>
     </form>
