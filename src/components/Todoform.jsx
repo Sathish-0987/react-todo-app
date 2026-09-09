042cc8815b5756d0b439c8e9ebfc6c8421c8aeef
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-09 16:59:14 +0530

    Added the UseState Hook

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Todoform.css"
 
 export default function Todoform() {
+  const [task,setTask]=useState("");
+  const handleSubmit = (e) => { e.preventDefault(); 
+    console.log(task);
+     setTask('');
+  };
   return (
-    <form className='todo-form'>
-        <input type='text' placeholder='Enter a Task'/>
+    <form className='todo-form' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Enter a Task' value={task} onChange={(e)=>setTask(e.target.value)}/>
         <button type='submit'>Add</button>
     </form>
   )
