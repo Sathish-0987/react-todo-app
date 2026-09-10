88653f05a5b7349bf3495fa8ee4c3e1a63fc6e29
Sathish-0987 <msathishkumar204@gmail.com>
2026-09-10 13:15:13 +0530

    Make the Task to print in web page

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -3,14 +3,27 @@ import "./Todoform.css"
 
 export default function Todoform() {
   const [task,setTask]=useState("");
+  const [todos,setTodos]=useState([]);
   const handleSubmit = (e) => { e.preventDefault(); 
-    console.log(task);
+    if (task.trim() === "") return;
+
+    setTodos([...todos, task]);
+    // console.log(task);
      setTask('');
   };
   return (
+    <>
     <form className='todo-form' onSubmit={handleSubmit}>
         <input type='text' placeholder='Enter a Task' value={task} onChange={(e)=>setTask(e.target.value)}/>
         <button type='submit'>Add</button>
     </form>
+    <div className="todo-list">
+
+        {todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
+
+      </div>
+      </>
   )
 }
